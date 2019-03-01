1df8cdf9a338498b273bebccca2cd077426365f5
PArth__PAtel <parthp474@gmail.com>
2019-03-01 17:23:07 +0530

    +fn for CIDv0

--- src/IPFSSpec.hs
+++ src/IPFSSpec.hs
@@ -1,4 +1,5 @@
 import qualified Data.ByteString as BS
+import MultiFormats as MF --Link with old project
 
 data CID = 	CIDv0 String -- always starts with Qm
 		|	CIDv1 String
@@ -15,3 +16,14 @@ data IPFSObj = 	IPFSObj
 					content :: BS.ByteString,
 					links :: [ IPFSLink ]
 				}
+
+
+-- Returns CIDv0 for content
+-- CIDv0 is base58 encoded sha256 hash of data
+-- cidv0 ::= <multihash-content-address>
+getCIDv0 ::		BS.ByteString -- content
+			-> 	CID -- calcualted CID, always starts with Qm
+
+getCIDv0 content =	CIDv0 cid
+			where
+				cid = MF.multiBase "base58" . MF.multiHash "SHA256" content -- TODO: replace with own implementation
