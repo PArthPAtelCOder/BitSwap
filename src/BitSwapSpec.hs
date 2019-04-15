2a10f1ab6f946f69c8f23dc48a6057aed563bb49
PArth__PAtel <parthp474@gmail.com>
2019-04-15 13:20:44 +0530

    +CIDv0 cal and validation, +msgSerialization, minor data modification in Peer & BSNodeState

--- src/BitSwapSpec.hs
+++ src/BitSwapSpec.hs
@@ -1,9 +1,12 @@
 module BitSwapSpec where
 
 import Data.Time.Clock as T
-import qualified Data.HashTable as H
+import qualified Data.Map as M
 import qualified Data.ByteString as BS
-import IPFSSpec
+import qualified Data.ByteString.Char8 as DBC
+import IPFSSpec as IPFS
+import IPFSSpec (Key, ID, NodeID)
+-- import IPFSSpec 
 
 
 -- Questions
@@ -11,34 +14,72 @@ import IPFSSpec
 
 -- TODO: implement VarInt
 
+-- difficulty
+-- alpha
+-- k
 
-	-- difficulty
-	-- alpha
-	-- k
-
---------- CONSTANTS --------
+--------- CONSTANTS ------------------------------------------
 
 -- Blocked peer is ignored for ignore_cooldown (in sec) period
 -- Default = 10s
 ignore_cooldown = 10
 
-
 -- connection that is idle for silence_wait (in sec) is closed
 -- Defalult = 30s
-silence_wait = 30 
+silence_wait = 30
 
-----------------------------
+-- delimiter to decide Msg's Boundary
+msgDelimiter = "\r\n\r\n"
+--------------------------------------------------------------
+			-- Testing Data --
+--------------------------------------------------------------
+objs =
+  [ IPFSObj (DBC.pack "kQ7qwL8kGhVLB1WVFXxR") []
+  , IPFSObj (DBC.pack "YCHKLFNf6Q2PTtzBUkdu") []
+  , IPFSObj (DBC.pack "frSc4P5V73TlbWx9VhTu") []
+  , IPFSObj (DBC.pack "rmxR8fJvs8eWiPnesOMt") []
+  ]
+
+storage = Prelude.foldl f M.empty objs
+	where
+		f x y = insert (getCIDv0 y) y x
+--------------------------------------------------------------
+
+data Priority = Low
+            |   Normal
+            |   High
+            
+            deriving (Enum, Ord, Eq, Show, Read)
+
+data Entry = Entry {
+                    cid :: IPFS.CID ,	-- CID of content
+                    priority :: Priority ,	-- Priority for Data Request, Higher Priority
+                    cancel :: Bool			-- True: Content is no more needed
+                    } deriving (Enum, Eq, Ord, Show, Read)
+
+type WantList = [ IPFS.CID ]
+type MsgQueue = [ Msg ]
+
+-- User interface to add request to wantList
+-- TODO     
+
+-- Whenever a new modification function is called for wantList, wantListMsg is changed accordingly.
+
 
 
 -- State of Node running BitSwap Protocol
 data BSNodeState = BSNodeState
 			{
-				ledgers :: H.HashTable (ID NodeID) (Ledger),
-				activePeers :: H.HashTable (ID NodeID) Peer,
+				ledgers :: M.Map (IPFS.ID NodeID) (Ledger),
+				activePeers :: M.Map (IPFS.ID NodeID) Peer,
 				nodeWantList :: WantList,
-				haveList :: H.HashTable (ID NodeID) IPFSSpec.IPFSObj,
-				blockList :: [ ID NodeID ]
+				haveList :: M.Map IPFS.CID IPFS.IPFSObj,
+				blockList :: [ IPFS.ID NodeID ],
+				currentWantListMsg :: Msg
 			}
+
+emptyBSNodeState = BSNodeState M.empty M.empty [] storage [] (WantListMsg [])
+
 
 isPeerBlocked :: 	ID NodeID -- Peer ID
 			->	BSNodeState   -- State of Node
@@ -46,29 +87,46 @@ isPeerBlocked :: 	ID NodeID -- Peer ID
 
 isPeerBlocked peerID state = elem peerID $ blockList state
 
-data WantList 	=	WantList 
-				{
-					lastUpdated		::	T.UTCTime ,
-					cids 			:: 	[ CID ]
-				} deriving (Show, Read)
+
+isContentAvailable :: 	IPFS.CID
+					->	BSNodeState
+					->	Bool
+
+isContentAvailable cid state =
+	let hl = haveList state
+	in	M.member cid hl
+
+
 
 -- Msg Formats
-data Msg = 	WantListMsg	WantList
+data Msg = 	WantListMsg [ Entry ]
 		|	OpenMsg 	Ledger
-		|	BlockMsg 	CID 	IPFSSpec.IPFSObj
+		|	BlockMsg 	CID 	IPFS.IPFSObj
+			
 			deriving (Show, Read, Eq)
+
+serializeMsg :: Msg -> BS.ByteString
+serializeMsg msg = DBC.pack $ (show msg) ++ delimiter
+
+deserializeMsg :: BS.ByteString -> Msg
+deserializeMsg binaryMsg = Read $ DBC.unpack binaryMsg
+
 
 -- Peer Information
 data Peer = Peer
 		{
-			peerID :: ID NodeID,
-			ledger :: Ledger,
-			-- timestamp of last succesful message communication
-			-- Used for connection timeout
-			lastSeen :: T.UTCTime,
+			peerID_p :: ID NodeID,			
+
+			ledger :: Ledger,				-- Latest Ledger of Peer
+											-- May not be same as in Archive
+
+			lastSeen :: T.UTCTime,			-- timestamp of last succesful message communication
+											-- Used for connection timeout
+
 			peerWantList :: WantList,
-			sendList :: [ CID ], -- CIDs of Block that we have
-			msgQueue :: [ Msg ], -- Messages to be sent, TODO: chan?
+			sendList :: [ IPFS.CID ], 	-- CIDs of Blocks that we have
+			
+			msgQueue :: MsgQueue 			-- Messages to be sent, TODO: chan?
 		}
 
 
@@ -76,7 +134,7 @@ data Peer = Peer
 data Ledger = Ledger
 			{
 				nodeID :: ID NodeID,
-				peerID :: ID NodeID,
+				peerID_l :: ID NodeID,
 				bytesRcvd :: Integer,
 				bytesSent :: Integer,
 				timeStamp :: UTCTime -- timestamp of last succesful block message communication
@@ -86,10 +144,10 @@ data Ledger = Ledger
 -- checking for equality is not good
 -- It should be difference < threshold
 instance Eq Ledger where
-	(==) x y = 	if 	(nodeID x) == (peerID y) &&
-					(nodeID y) == (peerID x) &&
-					(bytesSent x) == (bytesRcvd y) &&
-					(bytesSent y) == (bytesRcvd x) &&	then True
+	(==) x y = 	if 		(nodeID x) == (peerID y) 
+					&& 	(nodeID y) == (peerID x)
+					&&  (bytesSent x) == (bytesRcvd y)
+					&&  (bytesSent y) == (bytesRcvd x)	then True
 				else
 					False
 
@@ -109,85 +167,15 @@ newLedger ::	ID NodeID  -- Node ID
 			-> 	ID NodeID  -- Peer ID
 			->	Ledger     -- New Ledger
 
-newLedger peerID_ nodeID_ = Ledger ( nodeID_, peerID_, 0, 0, T.getCurrentTime )import Data.Time.Clock as CLK
-import qualified Data.HashTable as H
-import qualified Data.ByteString as BS
+newLedger peerID_ nodeID_ = Ledger ( nodeID_, peerID_, 0, 0, T.getCurrentTime )
 
+-- Returns as it is if exists else returns fresh ledger
+fetchLedgerFromArchive :: BSNodeState -> ID NodeID -> Ledger
 
--- CONSTANTS to be added
-	-- ignore_cooldown
-	-- difficulty
-	-- alpha
-	-- k
-	-- silence_wait = 30 default
-
--- Confirm ways to handle constants
--- Using type class
--- directly as a variable
-
-
--- Questions
--- 1) How to handle different version of CIDs as a Key
-
--- TODO: implement VarInt
-
-
-
--- State of Node running BitSwap Protocol
-data BitSwap = BitSwap
-			{
-				ledgers :: H.HashTable (ID NodeID) (Ledger),
-				activePeers :: H.HashTable (ID NodeID) Peer,
-				wantList :: [ CID ],
-				haveList :: H.HashTable (ID NodeID) IPFSObj
-				-- TODO: blockList
-			}
-
--- Information related to peer
-data Peer = Peer
-		{
-			peerID :: ID NodeID,
-			ledger :: Ledger,
-			-- timestamp of last succesful message communication
-			-- Used for connection timeout
-			lastSeen :: IO UTCTime,
-			wantList :: [ CID ]
-		}
-
-
-data Ledger = Ledger
-			{
-				nodeID :: ID NodeID,
-				peerID :: ID NodeID,
-				bytesRcvd :: Integer,
-				bytesSent :: Integer,
-				timeStamp :: IO UTCTime -- timestamp of last succesful block message communication
-			}
--- TODO: add logic for timeStamp comparision
--- checking for equality is not good
--- It should be difference < threshold
-instance Eq Ledger where
-	(==) x y = 	if 	(nodeID x) == (peerID y) &&
-					(nodeID y) == (peerID x) &&
-					(bytesSent x) == (bytesRcvd y) &&
-					(bytesSent y) == (bytesRcvd x) &&	then True
-				else
-					False
-
-
--- Resets Ledger
-resetLedger :: 		Ledger -- Old Ledger
-				-> 	Ledger -- New Ledger
-
-resetLedger ledger = ledger {
-								bytesRcvd = 0,
-								bytesSent = 0,
-								timeStamp = CLK.getCurrentTime
-							}
-
--- Create en empty ledger
-newLedger ::	ID NodeID  -- Node ID
-			-> 	ID NodeID  -- Peer ID
-			->	Ledger     -- New Ledger
-
-newLedger peerID_ nodeID_ = Ledger ( nodeID_, peerID_, 0, 0, CLK.getCurrentTime )
+fetchLedgerFromArchive state pId = 
+	let ledgers_ = ledgers state
+		nId = nodeID state
+		pLedger = M.lookup pId ledgers_
+	in
+		case pLedger of Nothing -> newLedger pId nId
+			            Just l -> l
--- src/IPFSSpec.hs
+++ src/IPFSSpec.hs
@@ -1,20 +1,31 @@
 module IPFSSpec where
-	
+
 
 import qualified Data.ByteString as BS
-import MultiFormats as MF --Link with old project
+import qualified Data.ByteString.Char8 as DBC
+import Crypto.Hash.SHA256 (hash)
+import Data.Word (Word8)
+-- import MultiFormats as MF --Link with old project
+import Data.ByteString.Base58 (encodeBase58, decodeBase58, bitcoinAlphabet)
 
 
+data CID 	= 	CIDv0 BS.ByteString -- always starts with Qm
+									-- CIDv0 is base58 encoded sha256 hash of data
+									-- cidv0 ::= <multihash-content-address>
+			|	CIDv1 BS.ByteString
 
-data CID = 	CIDv0 String -- always starts with Qm
-		|	CIDv1 String
+		deriving ( Show, Read, Eq, Ord )
 
-data IPFSLink = IPFSLink
+
+data IPFSLink = IPFSLink 
 				{
-					name :: String,
-					size :: Integer,
+					name :: String  ,
+					size :: Integer ,
 					cid :: CID
-				}
+				} 
+
+				deriving ( Show, Read, Eq )
+
 
 data IPFSObj = 	IPFSObj
 				{
@@ -22,28 +33,75 @@ data IPFSObj = 	IPFSObj
 					links :: [ IPFSLink ]
 				}
 
+				deriving ( Show, Read, Eq )
 
--- Returns CIDv0 for content
--- CIDv0 is base58 encoded sha256 hash of data
--- cidv0 ::= <multihash-content-address>
-getCIDv0 ::		BS.ByteString -- content
-			-> 	CID -- calcualted CID, always starts with Qm
+-- A phantom type to represent ID
+type ID a = BS.ByteString
 
-getCIDv0 content =	CIDv0 cid
-			where
-				cid = MF.multiBase "base58" . MF.multiHash "SHA256" content -- TODO: replace with own implementation
+data NodeID
+
+data Key
+
+-- | Temp MultiHash implmentation for Sha-256 encoding
+
+hashSHA256_MH 	:: 	BS.ByteString 	-- Content
+				->	BS.ByteString 	-- SHA-256 digest with prfix 0x12
+
+hashSHA256_MH content 	=	BS.append prefix digest
+	
+	where
+		prefix 		= BS.pack [18::Word8, 32::Word8] -- 18:Sha-256, 32:length of digest
+		digest 		= hashSHA256 content
+		hashSHA256 	= hash
 
 
--- Returns CIDv1 for data
--- <cidv1> ::= <multibase-prefix><cid-version><multicodec-content-type><multihash-content-address>
-getCIDv1 ::		BS.ByteString -- Data
-			->	HashFunction
-			->	CodecType
-			->	EncodingScheme
-			-> 	CID -- calcualted CIDv1
+-- | Returns CIDv0 for IPFSObj
+getCIDv0 ::		IPFSObj
+			-> 	CID 		--	calcualted CID, always starts with Qm
 
-getCIDv1 content hf ct es 	= 	CIDv1 cid
-							where
-								-- TODO: replace with own implementation
-								cidWithoutEncoding = multiCodec ct . multiHash hf content
-								cid = multiBase es (byte 1 + cidWithoutEncoding)
+getCIDv0 obj =	CIDv0 cid
+
+	where
+		content = DBC.pack $ show obj	
+		cid = encodeBase58 bitcoinAlphabet $ hashSHA256_MH content
+
+-- | Checks integrity of received IPFSObj
+verifyCID	::	IPFSObj		-- Received IPFSObj
+			->	CID 		-- Received CID
+			->	Bool		-- Result
+
+verifyCID obj cid_@(CIDv0 _ )	=	cid_ == getCIDv0 obj
+verifyCID obj cid_@(CIDv1 _ )	=   False -- TODO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- -- Returns CIDv1 for data
+-- -- <cidv1> ::= <multibase-prefix><cid-version><multicodec-content-type><multihash-content-address>
+-- getCIDv1 ::		IPFSObj
+-- 			->	HashFunction  	-- Hash function
+-- 			->	CodecType	  	-- Codec representing content serialization
+-- 			->	EncodingScheme 	-- Encoding to be used for CID
+-- 			-> 	CID 			-- calcualted CIDv1
+
+-- getCIDv1 content hf ct es 	= 	CIDv1 cid
+-- 							where
+-- 								-- TODO: replace with own implementation
+-- 								cidWithoutEncoding = multiCodec ct . multiHash hf content
+-- 								cid = multiBase es (byte 1 + cidWithoutEncoding)
